66b966983afb0c0cc282a870292c1341d81e61e2
bullshmaiser <bull@email.ua>
2025-04-17 16:22:13 +0300

    Update Hero.tsx

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -1,28 +1,25 @@
-// components/Hero.tsx
+'use client'
+
 import Image from "next/image";
 import { FC } from "react";
 
 const Hero: FC = () => {
   return (
-    <section className="hero">
-      <div className="hero-content">
-<h1>Create unique personalized books for anyone!</h1>
-<p>
-Upload photos, craft a storyline, and our AI will transform them into a stunning illustrated book tailored to your ideas and wishes.
-</p>
-<button>Create a Book</button>
-      </div>
-      <div className="hero-image">
-        <Image
-          src="/hero-background.svg" 
-          alt="Logo"
-          width={460} 
-          height={312} 
-        />
+    <section className="hero bg-cover bg-center relative w-full h-screen" style={{ backgroundImage: 'url(/hero-background.svg)' }}>
+      {/* Контейнер для текста */}
+      <div className="absolute inset-0 flex flex-col items-center justify-center text-center text-white px-8 py-16 sm:px-16 sm:py-32">
+        <h1 className="text-3xl sm:text-5xl font-semibold mb-4">
+          Create unique personalized books for anyone!
+        </h1>
+        <p className="text-lg sm:text-xl mb-6">
+          Upload photos, craft a storyline, and our AI will transform them into a stunning illustrated book tailored to your ideas and wishes.
+        </p>
+        <button className="bg-black text-white py-2 px-6 rounded hover:bg-gray-800">
+          Create a Book
+        </button>
       </div>
     </section>
   );
 };
 
 export default Hero;
-
